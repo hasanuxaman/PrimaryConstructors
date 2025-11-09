af5a9e90613923bbd4d0b7d5a1e655acc085dcab
Md Hasanuzzaman Rony <hasanuxaman.rony@hotmail.com>
2025-11-09 14:51:07 +0600

    InMemoryDatabase_Asp>Net

--- PrimaryConstructors/Service/ProductService.cs
+++ PrimaryConstructors/Service/ProductService.cs
@@ -1,16 +1,21 @@
-﻿using PrimaryConstructors.DTOs;
+﻿using Microsoft.EntityFrameworkCore;
+using PrimaryConstructors.DTOs;
 using PrimaryConstructors.Interface;
 using PrimaryConstructors.Models;
 
 namespace PrimaryConstructors.Service
 {
-    public class ProductService(ILogger<ProductService> logger)
+    public class ProductService(ILogger<ProductService> logger, ApplicationDbContext context)
     : IProductService
     {
+
+       
         public async Task<List<Product>> GetAllAsync()
         {
             logger.LogInformation("Getting all products");
-            return  new  List<Product>() ;
+            var products = await context.Products.ToListAsync();
+
+            return products;
         }
 
         public Task<bool> DeleteAsync(int id)
@@ -30,9 +35,19 @@ namespace PrimaryConstructors.Service
             throw new NotImplementedException();
         }
 
-        public Task<Product> CreateAsync(CreateProductRequest request)
+        public async Task<Product> CreateAsync(CreateProductRequest request)
         {
-            throw new NotImplementedException();
+            var product = new Product(
+                0, 
+                request.Name,
+                request.Description,
+                request.Price
+            );
+
+            context.Products.Add(product);
+            await context.SaveChangesAsync();
+
+            return product;
         }
     }
 }
